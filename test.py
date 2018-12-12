3ee8325915df894f06c714ec6fdab5283b9b2f38
Karan <hydroweaver@gmail.com>
2018-12-12 14:30:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,3 +8,7 @@ new edit:
     pushed branch to github
     then making this current change
     save
+
+new edit:
+    understood that git checkout changes the local file based on the branch etc or multiple files if thats the case
+    now adding this line  online to check git pull functionality
